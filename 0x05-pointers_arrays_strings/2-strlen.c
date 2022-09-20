b1dafaa560dc0ec8dd578667f389dba8e89ad8f3
abdirahmanabdi <abdirahmanabdi.ma@gmail.com>
2022-09-20 15:50:50 -0700

    done

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,17 @@
 #include "main.h"
 
 /**
- * reset_to_98 - resets the value of given integer to 98
- * @n: parameter
- * Return : nothing
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: returns length as integer;
  */
 
-void reset_to_98(int *n)
+int _strlen(char *s)
 {
-	*n = 98;
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+
+	return (len);
 }
