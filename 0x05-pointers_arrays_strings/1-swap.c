08424b1348a6bb1f8d8fc4a5d0ae8fdb1a149b3d
abdirahmanabdi <abdirahmanabdi.ma@gmail.com>
2022-09-20 15:45:51 -0700

    done

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,21 +1,16 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
 /**
- * void prototypes()
- * int prototypes()
+ * swap_int - swaps two integers' values
+ * @a: first integer
+ * @b: second integer
+ * Return: returns nothing
  */
 
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *str);
+void swap_int(int *a, int *b)
+{
+	int tmp = *a;
 
-#endif /*MAIN_H*/
+	*a = *b;
+	*b = tmp;
+}
